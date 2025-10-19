61a80ad3dd318550fea73405d57164bc1e1c49da
Bibek Dhara <157223258+TheCurryGuy@users.noreply.github.com>
2025-10-19 11:19:32 +0530

    Delete machine_learning/Model-1 directory

--- machine_learning/Model-1/main.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def generate_teams(df: pd.DataFrame, output_path: str):
-    # Example logic
-    df["team_number"] = range(1, len(df) + 1)
-    df.to_csv(output_path, index=False)
